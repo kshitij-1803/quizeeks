077213f5cdd0373c5e0e15e93d5040672e78c110
Chirag <chirag9893@gmail.com>
2020-08-23 23:21:55 +0530

    highscores list added

--- app.js
+++ app.js
@@ -17,11 +17,20 @@ app.set("view engine","ejs") ;
 var userSchema= new mongoose.Schema({
     username: String,
     score: String
-   });
+   }, { retainKeyOrder: true });
    var user = mongoose.model("uscore",userSchema);
 
 
+app.get("/highscores",function(req,res){ 
+    user.find({},function(err,uscore){
+        if(err)
+            console.log(err) ;
+        else    
+        res.render("highscores",{uscore:uscore}) ;
 
+    })
+    
+});
 app.get("/",function(req,res){ 
         res.render("index") ;
 
@@ -33,20 +42,13 @@ app.get("/end",function(req,res){
         res.render("end") ;
 
 });
-app.get("/highscores",function(req,res){ 
-        res.render("highscores") ;
-
-});
 
 
 app.post("/end",function(req,res){ 
       var obj={
       username:  req.body.username,
       score: req.body.score 
-    }
-console.log("Score is     :?" ,req.body);
-console.log("Score is     :?" ,req.body.score);
-
+     }
     user.create(obj,function(err,user){
             if(err)
                 console.log(err) ;
@@ -55,9 +57,59 @@ console.log("Score is     :?" ,req.body.score);
             }) ;
  res.render("index") ;
 });
-// app.post("/end",function(req,res){ 
-// 	res.render("index") ;
-// }) ;
+
+// app.get("/highscores",function(req,res){ 
+//     user.find({},null,{sort: {score: 1}},function(err,uscore){
+//         if(err)
+//             console.log(err) ;
+//         else    
+//         res.render("highscores",{uscore:uscore}) ;
+
+//     })
+    
+// });
+
+ app.get("/highscores",function(req,res){
+
+var mysort = {score: -1};
+
+    user.find({}, function (err, result) {
+
+        if (err) {
+
+            console.log(err);
+
+        } else {
+
+              res.render("highscores",{uscore:uscore}) ;
+
+        }
+
+    }).sort(mysort);
+
+ }) ;
+
+
+
+
+
 app.listen(5500,function(){
     console.log("Server Started" );              
 });
+
+
+// var mysort = {name: 1};
+
+//     user.find({}, function (err, result) {
+
+//         if (err) {
+
+//             console.log("error query");
+
+//         } else {
+
+//               res.render("highscores",{uscore:uscore}) ;
+
+//         }
+
+//     }).sort(mysort);
