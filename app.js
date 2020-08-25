3aef999c02ebb83b4abd125bd304ee781f0b9f12
Kshitij Thatte <kshitijthatte@gmail.com>
2020-08-25 23:40:42 +0530

    updated UI for login & register pages

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express =require('express');
-var app=express() ;
-var mongoose= require("mongoose") ;
-var path= require('path') ;
-var bcrypt= require('bcrypt') ;
-var bodyParaser= require("body-parser") ;
-var passport=require("passport") ;
-var data=require('./models/user.js') ;
-var flash=require('express-flash') ;
-var session=require('express-session') ;
-var LocalStrategy=require('passport-local') ;
-var passportLocalMongoose=require('passport-local-mongoose') ;
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var path = require("path");
+var bcrypt = require("bcrypt");
+var bodyParaser = require("body-parser");
+var passport = require("passport");
+var data = require("./models/user.js");
+var flash = require("express-flash");
+var session = require("express-session");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 //var initializePassport=require("./passport-config") ;
-var methodOverride = require('method-override') ;
+var methodOverride = require("method-override");
 
-var er ="" ;
+var er = "";
 // initializePassport(
 //   passport,
 //   email=>users.find(user=>user.email===email),
@@ -22,36 +22,41 @@ var er ="" ;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/quizeeks",({ useNewUrlParser: true,useUnifiedTopology: true  }));
+mongoose.connect("mongodb://localhost/quizeeks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-app.use(bodyParaser.urlencoded({extended:true})) ;
+app.use(bodyParaser.urlencoded({ extended: true }));
 
-app.use(flash()) ;
+app.use(flash());
 
-app.use(session({
-  secret:"Quizeeks will be the best" ,
-  resave:false,
-  saveUnitialized:false 
-}));
+app.use(
+    session({
+        secret: "Quizeeks will be the best",
+        resave: false,
+        saveUnitialized: false,
+    })
+);
 
-app.use(passport.initialize()) ;
-app.use(passport.session()) ;
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set("view engine","ejs") ;
+passport.use(new LocalStrategy(data.authenticate()));
+passport.serializeUser(data.serializeUser());
+passport.deserializeUser(data.deserializeUser());
 
-passport.use(new LocalStrategy(data.authenticate())); 
-passport.serializeUser(data.serializeUser()); 
-passport.deserializeUser(data.deserializeUser()); 
-
-
-var userSchema= new mongoose.Schema({
-    username: String,
-    score: String
-   }, { retainKeyOrder: true });
-   var usc = mongoose.model("uscore",userSchema);
-
+var userSchema = new mongoose.Schema(
+    {
+        username: String,
+        score: String,
+    },
+    { retainKeyOrder: true }
+);
+var usc = mongoose.model("uscore", userSchema);
 
 // const users=data.find({},function(err,uscore){
 //   if(err)
@@ -62,126 +67,109 @@ var userSchema= new mongoose.Schema({
 // passport.serializeUser(user.serializeUser() ) ;
 // passport.deserializeUser(user.deserializeUser()) ;
 
-
-
-app.get("/highscores",function(req,res){ 
-    usc.find({},function(err,uscore){
-        if(err)
-            console.log(err) ;
-        else    
-        res.render("highscores",{uscore:uscore}) ;
-  })
+app.get("/highscores", function (req, res) {
+    usc.find({}, function (err, uscore) {
+        if (err) console.log(err);
+        else res.render("highscores", { uscore: uscore });
+    });
 });
 
-
-app.get("/",checkAuthenticated,function(req,res){ 
-        res.render("index",{user:req.user}) ;
-
+app.get("/", checkAuthenticated, function (req, res) {
+    res.render("index", { user: req.user });
 });
-app.get("/game",checkAuthenticated,function(req,res){ 
-        res.render("game") ;
+app.get("/game", checkAuthenticated, function (req, res) {
+    res.render("game");
 });
-app.get("/end",checkAuthenticated,function(req,res){ 
-        res.render("end",{user:req.user}) ;
+app.get("/end", checkAuthenticated, function (req, res) {
+    res.render("end", { user: req.user });
 });
-app.get("/highscores",checkAuthenticated,function(req,res){
-
-const mysort = {username: -1};
+app.get("/highscores", checkAuthenticated, function (req, res) {
+    const mysort = { username: -1 };
 
     user.find({}, function (err, result) {
-
         if (err) {
-
             console.log(err);
-
         } else {
-              res.render("highscores",{uscore:uscore}) ;
-     }
+            res.render("highscores", { uscore: uscore });
+        }
     }).sort(mysort);
-
- }) ;
-
-app.get("/login",checkNotAuthenticated,function(req,res){ 
-        res.render("login") ;
 });
 
-
-app.get("/register",checkNotAuthenticated,function(req,res){ 
-        res.render("register") ;
+app.get("/login", checkNotAuthenticated, function (req, res) {
+    res.render("login");
 });
 
-
-app.post("/login", passport.authenticate("local", { 
-  successRedirect: "/", 
-  failureRedirect: "/login"
-}), function (req, res) { 
-}); 
-
-
-
-app.post("/end",checkAuthenticated,function(req,res){ 
-      var obj={
-      username:  req.body.username,
-      score: req.body.score 
-     }
-    usc.create(obj,function(err,user){
-            if(err)
-                console.log(err) ;
-            else    
-                console.log("Added sucesfully ",user) ;
-            }) ;
- res.render("index",{user:req.user}) ;
+app.get("/register", checkNotAuthenticated, function (req, res) {
+    res.render("register");
 });
 
-app.post("/register", function (req, res) { 
- 
-  data.register(new data({username:req.body.username,name:req.body.name}), req.body.password, function (err, user) { 
-      if (err) { 
-        var errors = err;
-          console.log(err); 
-          return res.render("register",{error: err}); 
-      } 
-      passport.authenticate("local")( req, res, function () { 
-          res.render("login"); 
-      }); 
-  }); 
-}); 
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+    }),
+    function (req, res) {}
+);
 
-// app.get("/highscores",function(req,res){ 
+app.post("/end", checkAuthenticated, function (req, res) {
+    var obj = {
+        username: req.body.username,
+        score: req.body.score,
+    };
+    usc.create(obj, function (err, user) {
+        if (err) console.log(err);
+        else console.log("Added sucesfully ", user);
+    });
+    res.render("index", { user: req.user });
+});
+
+app.post("/register", function (req, res) {
+    data.register(
+        new data({ username: req.body.username, name: req.body.name }),
+        req.body.password,
+        function (err, user) {
+            if (err) {
+                var errors = err;
+                console.log(err);
+                return res.render("register", { error: err });
+            }
+            res.render("login");
+        }
+    );
+});
+
+// app.get("/highscores",function(req,res){
 //     user.find({},null,{sort: {score: 1}},function(err,uscore){
 //         if(err)
 //             console.log(err) ;
-//         else    
+//         else
 //         res.render("highscores",{uscore:uscore}) ;
 
 //     })
-    
+
 // });
 
- app.get('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
- 
+app.get("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/login");
+});
 
 function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
+    if (req.isAuthenticated()) {
+        return next();
+    }
 
-  res.redirect('/login')
+    res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
+    if (req.isAuthenticated()) {
+        return res.redirect("/");
+    }
+    next();
 }
 
-
-
-
-app.listen(5500,function(){
-    console.log("Server Started" );              
+app.listen(5500, function () {
+    console.log("Server Started");
 });
