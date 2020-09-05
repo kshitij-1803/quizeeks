566bffa4a9cbdc3ec492d09cb6bfb9566f9bd573
Chirag <chirag9893@gmail.com>
2020-09-05 22:37:27 +0530

    creator can see their custom quizes and no one else can

---
+++ public/javascripts/highscores.js
@@ -0,0 +1,13 @@
+// const highScoresList = document.getElementById("highScoresList");
+// const highScores = JSON.parse(localStorage.getItem("highScores"));
+//    var sc=document.getElementById("Score");
+
+// highScoresList.innerHTML = highScores
+//     .map((score) => {
+//         return `<li class="high-score">${score.name} - ${score.score}</li>`;
+//     })
+//     .join("");
+
+
+
+//    sc.innerHTML=highScores ;
